8e25b155b279db6cd15a9f695466e5fe92b6d3f0
Ndubuisi Onyemenam <onyemenamndu@gmail.com>
2018-10-27 20:42:48 +0100

    Change mail option

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -120,7 +120,4 @@ return [
         ],
     ],
 
-],
-
 ];
-
